b1fb467eaf7a95e8051b3e7308082366d13463aa
NatashaMuhanji <natashamuhanji@gmail.com>
2024-12-06 17:16:41 +0300

    adding the grids

---
+++ script.js
@@ -0,0 +1,36 @@
+const container = document.querySelector(".container");
+
+//reusable function to create the grid so we can adjust its size dynamically
+function createGrid(size) {
+    //clear container to prevent overlapping of grids
+    // container.innerHTML = '';
+
+    container.style.display = 'flex';
+    container.style.flexWrap = 'wrap';
+    container.style.width= '960px';
+    container.style.height= '960px';
+    container.style.border= '2px solid black';
+    container.style.justifyContent = 'center';
+
+    //calculate the sice of each square
+    const squareSize = 960/(size + 1);
+
+    //loop for creating the squares
+    for (i= 0; i < size*size; i++) {
+        //creating new div
+        const square = document.createElement("div");
+
+        //putting div inside container
+        container.append(square);
+
+        //adding class for styling
+        square.classList.add("square");
+
+        square.style.border = "1px solid black";
+        square.style.height = `${squareSize}px`;
+        square.style.width = `${squareSize}px`;
+
+    }
+}
+
+createGrid(16);
